5330a2134347bdfc52c51e3bd92586bf946774b6
段威 <dovaove@163.com>
2016-09-29 15:39:49 +0800

    upload public App ID and Key

--- Android/app/src/main/java/cn/leancloud/leanstoragegettingstarted/GettingStartedApp.java
+++ Android/app/src/main/java/cn/leancloud/leanstoragegettingstarted/GettingStartedApp.java
@@ -12,7 +12,7 @@ public class GettingStartedApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    AVOSCloud.initialize(this,"你的 App ID", "你的 App Key");
+    AVOSCloud.initialize(this,"OLoj899IwHYi787ClrImlr3k-gzGzoHsz", "gkz35mRTqTE2aqwp7dEr5uEE");
     AVOSCloud.setDebugLogEnabled(true);
   }
 }
